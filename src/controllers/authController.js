47c48bb34a3e584c1fdea10c5a6d038b3ab37e8e
Siddiq3 <siddiqkolimi.dev@gmail.com>
2024-05-23 01:23:47 +0530

    rentify message

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Users = require('../db/model/users');
 
+
+const { v4: uuidv4 } = require('uuid');
+const secretKey = uuidv4();
+console.log(secretKey); // Output: a randomly generated UUID
+
 exports.register = async (req, res) => {
   try {
     const { firstName, lastName, email, phoneNumber, password, role } = req.body;
@@ -23,9 +28,10 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id }, secretKey, { expiresIn: '1h' });
     res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
